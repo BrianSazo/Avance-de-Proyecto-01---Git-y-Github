bdb35c6cc37fb618125eb2b5a1b1751dbe44b3d5
Brian <149214500+BrianSazo@users.noreply.github.com>
2024-08-09 11:29:32 -0600

    Add files via upload

---
+++ Quetzal_Dolar.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author sazo
+ */
+/**
+ * Permite calcular la conversion de quetzales a dolares
+ */
+public class Quetzal_Dolar {
+    
+       public static void main(String[] args) {
+        /**
+         * se Definen las variables como Reales
+         */
+        double Quetzales, Dolares, Total;
+        Scanner scanner = new Scanner(System.in);
+        /**
+         * se le solicita al usuario que coloque la cantidad de Quetzales
+         */
+        System.out.println("Coloque la cantidad de Quetzales:");
+        Quetzales = scanner.nextDouble();
+        /**
+         * Se le solicita al usuario que coloque el precio en que esta el dolar en ese momento
+         */
+        System.out.println("Coloque el precio del Dólar:");
+        Dolares = scanner.nextDouble();
+        /**
+         * se hace la operacion para convertir los quetzales a dolares
+         */
+        Total = Quetzales / Dolares;
+        /**
+         * se muestra la cantidad de dolares equivalente a la cantidad de quetzales
+         */
+        System.out.println("La cantidad de dólares es: " + Total);
+    }
+}
